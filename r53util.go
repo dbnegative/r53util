8b6d4967f0653d6ed02bf1699c4095f88ae2c7fb
dbnegative <shortjay@gmail.com>
2016-02-02 22:19:11 +0000

    import now works for JSON files created with r53util

--- r53util.go
+++ r53util.go
@@ -4,12 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
 )
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 var flagRegion string
 
@@ -21,12 +25,15 @@ type ZoneData struct {
 	RecordSetsParams []*route53.ListResourceRecordSetsInput
 }
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func main() {
 
 	//get cmdline args
 	arg := os.Args
 	argLength := len(arg)
-
 	//intialise
 	zones := &ZoneData{}
 
@@ -37,7 +44,7 @@ Done:
 			if argLength == 3 {
 				loadJSONFile(arg[2], zones)
 				zones.restoreHostedZone()
-
+				zones.restoreRecordSet()
 				//zones.outputJSON()
 				break Done
 			}
@@ -81,7 +88,6 @@ Done:
 			printHelp()
 			break Done
 		} //end of switch
-
 		if idx == argLength-1 {
 			fmt.Println("Error: command or variable incorrect or missing")
 			printHelp()
@@ -105,7 +111,6 @@ func outputJSONFile(filename string, zone ZoneData) {
 		fmt.Println(err.Error())
 		return
 	}
-
 	err = ioutil.WriteFile(filename, output, 0644)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -120,7 +125,6 @@ func loadJSONFile(filename string, zone *ZoneData) {
 		fmt.Println(err.Error())
 		return
 	}
-
 	err = json.Unmarshal(data, zone)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -133,62 +137,47 @@ func (zone *ZoneData) getHostedZones() {
 	svc := route53.New(session.New(), &aws.Config{Region: aws.String(flagRegion)})
 	resp, err := svc.ListHostedZonesByName(zone.HostedZoneParams)
 	if err != nil {
-		panic(err)
+		fmt.Println(err.Error())
 	}
 	//Initialise
 	zone.HostedZone = make([]*route53.HostedZone, len(resp.HostedZones))
 	for k, v := range resp.HostedZones {
 		//Populate
 		zone.HostedZone[k] = v
-
 	}
 }
 
 //getRecordSets - Get all recordsets from associated host zones
 func (zone *ZoneData) getRecordSets() {
-
 	svc := route53.New(session.New(), &aws.Config{Region: aws.String(flagRegion)})
-
 	//Initialise
 	zone.RecordSetsParams = make([]*route53.ListResourceRecordSetsInput, len(zone.HostedZone))
-
 	for k := range zone.HostedZone {
-
 		//Initialise if this is the first call
 		if len(zone.RecordSets) == 0 {
-
 			zone.RecordSets = make([]*route53.ListResourceRecordSetsOutput, len(zone.HostedZone))
-
 			for i := range zone.HostedZone {
 				zone.RecordSets[i] = &route53.ListResourceRecordSetsOutput{}
 				zone.RecordSetsParams[i] = &route53.ListResourceRecordSetsInput{}
-				zone.RecordSetsParams[i].MaxItems = aws.String("2")
+				//zone.RecordSetsParams[i].MaxItems = aws.String("2")
 				zone.RecordSetsParams[i].HostedZoneId = zone.HostedZone[i].Id
 			}
 		}
-
 		resp, err := svc.ListResourceRecordSets(zone.RecordSetsParams[k])
-
 		if err != nil {
 			fmt.Println("Error: ", err)
 			break
 		}
-
 		zone.RecordSets[k] = resp
-
 		//deal with pagination
 		for *resp.IsTruncated == true {
-
 			zone.RecordSetsParams[k].StartRecordName = aws.String(*resp.NextRecordName)
 			zone.RecordSetsParams[k].StartRecordType = aws.String(*resp.NextRecordType)
-
 			resp, err = svc.ListResourceRecordSets(zone.RecordSetsParams[k])
-
 			if err != nil {
 				fmt.Println("Error: ", err)
 				break
 			}
-
 			zone.RecordSets[k].ResourceRecordSets = append(zone.RecordSets[k].ResourceRecordSets, resp.ResourceRecordSets[0])
 		}
 	}
@@ -196,58 +185,71 @@ func (zone *ZoneData) getRecordSets() {
 
 //restoreHostedZone - Restores a Hosted Zone
 func (zone *ZoneData) restoreHostedZone() {
-
 	svc := route53.New(session.New(), &aws.Config{Region: aws.String(flagRegion)})
-
 	params := &route53.CreateHostedZoneInput{
-		CallerReference: aws.String("hostedzonecreated2"),     // Required
+		CallerReference: aws.String(randStringBytes(5)),       // Required
 		Name:            aws.String(*zone.HostedZone[0].Name), // Required
 		//DelegationSetId: aws.String(""),
-		//HostedZoneConfig: zone.HostedZone[0].Config,
+		HostedZoneConfig: &route53.HostedZoneConfig{
+			Comment: aws.String(*zone.HostedZone[0].Config.Comment),
+		},
+		//None of the below is supported yet as associated VPC ID is not stored in
+		//any of the availble structs
 		//VPC:              &route53.VPC{
 		//VPCId:     aws.String("VPCId"),
 		//VPCRegion: aws.String("VPCRegion"),
 		//},
 	}
-
 	resp, err := svc.CreateHostedZone(params)
-
 	if err != nil {
 		// Print the error, cast err to awserr.Error to get the Code and
 		// Message from an error.
 		fmt.Println(err.Error())
 		return
 	}
-
-	fmt.Println(resp)
+	//Set host zone id so that records sets can be created in the right place
+	zone.HostedZone[0].Id = resp.HostedZone.Id
 }
 
 //restoreRecordSet - Restores record set to Zone
 func (zone *ZoneData) restoreRecordSet() {
 	svc := route53.New(session.New())
+	rrchanges := make([]*route53.Change, len(zone.RecordSets[0].ResourceRecordSets))
+	//Populate the batch of change requests
+	for k := range zone.RecordSets[0].ResourceRecordSets {
+		//These are normally auto generated but allow for custom settings thus they are overwritten
+		if *zone.RecordSets[0].ResourceRecordSets[k].Type == "NS" {
+			rrchanges[k] = &route53.Change{
+				Action:            aws.String("UPSERT"),
+				ResourceRecordSet: zone.RecordSets[0].ResourceRecordSets[k],
+			}
+		} else if *zone.RecordSets[0].ResourceRecordSets[k].Type == "SOA" {
+			rrchanges[k] = &route53.Change{
+				Action:            aws.String("UPSERT"),
+				ResourceRecordSet: zone.RecordSets[0].ResourceRecordSets[k],
+			}
+		} else { //All other record types
+			rrchanges[k] = &route53.Change{
+				Action:            aws.String("CREATE"),
+				ResourceRecordSet: zone.RecordSets[0].ResourceRecordSets[k],
+			}
+		}
+	}
 
 	params := &route53.ChangeResourceRecordSetsInput{
 		ChangeBatch: &route53.ChangeBatch{ // Required
-			Changes: []*route53.Change{ // Required
-				{ // Required
-					Action:            aws.String("ChangeAction"), // Required
-					ResourceRecordSet: zone.RecordSets[0].ResourceRecordSets[0],
-				},
-				// More values...
-			},
-			Comment: aws.String("ResourceDescription"),
+			Changes: rrchanges,
+			Comment: aws.String("Restored by r53util"),
 		},
 		HostedZoneId: aws.String(*zone.HostedZone[0].Id), // Required
 	}
 	resp, err := svc.ChangeResourceRecordSets(params)
-
 	if err != nil {
 		// Print the error, cast err to awserr.Error to get the Code and
 		// Message from an error.
 		fmt.Println(err.Error())
 		return
 	}
-
 	// Pretty-print the response data.
 	fmt.Println(resp)
 }
@@ -260,4 +262,12 @@ func (zone *ZoneData) outputJSON() {
 		fmt.Println(string(hostZoneOutput))
 		fmt.Println(string(recordSetOutput))
 	}
+}
+
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
